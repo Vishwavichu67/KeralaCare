0124f729047ce8a61c7d4f5cf0ea836d941413fa
B.S Sri Subeetchanya <srisubeetchanya2004@gmail.com>
2025-09-21 14:23:42 +0000

    Force dynamic rendering for book-appointment page1

--- src/app/book-appointment/page.tsx
+++ src/app/book-appointment/page.tsx
@@ -1,5 +1,5 @@
-export const dynamic = "force-dynamic";
 'use client';
+export const dynamic = "force-dynamic";
 import { useSearchParams } from 'next/navigation';
 import AppointmentScheduler from '@/components/appointments/AppointmentScheduler';
 import Header from '@/components/common/Header';
